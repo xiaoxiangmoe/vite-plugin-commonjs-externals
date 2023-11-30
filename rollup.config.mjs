fa4069110ef4a3fcc6cad79cada207b52289a98d
ZHAO Jin-Xiang <xiaoxiangmoe@gmail.com>
2023-11-30 21:57:47 +0800

    chore: improve typing

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,28 +1,15 @@
-import fs from 'node:fs';
+// @ts-check
+import * as fs from 'node:fs';
 import ts from 'rollup-plugin-typescript2';
 import dts from 'rollup-plugin-dts';
 
+/** @type {import('./package.json')} */
 const pkg = JSON.parse(fs.readFileSync('./package.json', 'utf-8'));
 
 const external = [
   ...Object.keys(pkg.dependencies),
   ...Object.keys(pkg.peerDependencies),
 ];
-
-/** @type { import('rollup').RollupOptions } */
-const dtsConfig = {
-  plugins: [
-    dts({
-      respectExternal: true,
-      compilerOptions: {
-        // see https://github.com/unjs/unbuild/pull/57/files
-        preserveSymlinks: false,
-      },
-    }),
-  ],
-  external,
-  input: './src/index.ts',
-};
 
 /** @type { Array<import('rollup').RollupOptions> } */
 const config = [
@@ -51,12 +38,21 @@ const config = [
     ],
   },
   {
-    ...dtsConfig,
-    output: [{ file: pkg.exports['.'].require.replace('.cjs', '.d.cts') }],
-  },
-  {
-    ...dtsConfig,
-    output: [{ file: pkg.exports['.'].import.replace('.mjs', '.d.mts') }],
+    plugins: [
+      dts({
+        respectExternal: true,
+        compilerOptions: {
+          // see https://github.com/unjs/unbuild/pull/57/files
+          preserveSymlinks: false,
+        },
+      }),
+    ],
+    external,
+    input: './src/index.ts',
+    output: [
+      { file: pkg.exports['.'].require.replace('.cjs', '.d.cts') },
+      { file: pkg.exports['.'].import.replace('.mjs', '.d.mts') },
+    ],
   },
 ];
 
